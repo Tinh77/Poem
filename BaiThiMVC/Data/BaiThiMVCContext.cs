9085b7a8eee506fadc42a68fb97100b61801b952
Tinh77 <tinhgiang9898@gmail.com>
2018-11-24 10:15:48 +0700

    push

---
+++ BaiThiMVC/Data/BaiThiMVCContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BaiThiMVC.Models;
+
+namespace BaiThiMVC.Models
+{
+    public class BaiThiMVCContext : DbContext
+    {
+        public BaiThiMVCContext (DbContextOptions<BaiThiMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BaiThiMVC.Models.Product> Product { get; set; }
+
+        public DbSet<BaiThiMVC.Models.Category> Category { get; set; }
+    }
+}
---
+++ BaiThiMVC/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaiThiMVC.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierId { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string UnitPrice { get; set; }
+        public string UnitslnStock { get; set; }
+        public string UnitsOnOrder { get; set; }
+        public string ReorderLevel { get; set; }
+        public string Discontinued { get; set; }
+
+
+    }
+}
